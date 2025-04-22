4e23a691021a26955fa9374556718f4e694bb53b
Veera Raghava <veeratellakula@gmail.com>
2025-04-22 13:54:03 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -3,22 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div
-      className="App-header"
-      style={{
-        backgroundImage: `url(${annapic})`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'right',
-      }}
-    >
+    <div className="App-header">
       <img src={annapic} className="App-logo" alt="logo" />
-      <h5> May all your dreams come true.  Wishing you endless happiness</h5>
-      <h3>On this special day, May your day be filled with joy, 
-      laughter, and unforgettable moments. Here's to another amazing year ahead✨🎉</h3>
-      
-      <h1 style={{ color: 'red' }}> Happy Birthday Supporter! 💖</h1>
-
+      <div className="App-message">
+        <h5>May all your dreams come true. Wishing you endless happiness</h5>
+        <h3>
+          On this special day, may your day be filled with joy, laughter, and unforgettable
+          moments. Here's to another amazing year ahead ✨🎉
+        </h3>
+        <h1>Happy Birthday Supporter! 😍💖</h1>
+      </div>
     </div>
   );
 }
